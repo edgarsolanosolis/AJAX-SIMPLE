4cb09281c83dd13369daba33d808a30930b10d71
edgarsolanosolis <bolivarsolano@hotmail.com>
2019-01-25 21:19:22 -0600

    Commit with comments

--- backend/backendGET.php
+++ backend/backendGET.php
@@ -1,5 +1,5 @@
-<?php
+<?php                       //script de php
 
-echo "Hello worl with AJAX";
+echo "Hello worl with AJAX"; //enviar un string, lo unico que hace el backend
 
 ?>
--- backend/backendPOST.php
+++ backend/backendPOST.php
@@ -1,7 +1,7 @@
-<?php
+<?php                       //script de php
 
 
-if(isset($_POST)){
+if(isset($_POST)){ //Si existe una peticion post, vamos a devolver el dato que recibimos.
     echo "Recived " . $_POST['username'];
 }
 
